dd004c177f4504df475d97744fedecb0a7164ec8
Akindu De Silva <desilvaakindupro@gmail.com>
2024-05-29 09:25:25 +0530

    Bubble sorting
    Bubble sort is a simple sorting algorithm that works by repeatedly iterating through an array. In each pass, it compares adjacent elements and swaps them if they are in the wrong order (larger element comes first). This process continues until no more swaps are needed, signifying the array is sorted in ascending order.

---
+++ BubbleSort.java
@@ -0,0 +1,54 @@
+package BubbleSort;
+import java.util.Scanner;
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		 // Get user input for the number of elements
+        System.out.print("Enter the number of elements to sort: ");
+        int n = scanner.nextInt();
+
+        // Create an array to store the elements
+        int[] arr = new int[n];
+
+        // Get user input for each element
+        System.out.println("Enter the elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+
+        }
+        
+        
+         bubbleSortMethod(arr);
+        
+        System.out.println("Sorted array: ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+
+	
+	// Perform bubble sort
+   
+
+    // Print the sorted array
+   
+// Function to perform bubble sort
+public static void bubbleSortMethod(int[] arr) {
+    int n = arr.length;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements if they are in the wrong order
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+}
